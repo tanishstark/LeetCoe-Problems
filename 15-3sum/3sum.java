a9fc78ddec8edbc0c81a8a39a43bda6af14c14c8
Tanish kumar <tanishstark1234@gmail.com>
2026-08-25 17:42:11 +0530

    Time: 32 ms (77.40%) | Memory: 58.5 MB (94.69%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -5,6 +5,10 @@ class Solution {
 
         for(int i = 0; i <nums.length -2; i++ ){
 
+        if(nums[i]>0){
+            break;
+        }
+
         if(i > 0 && nums[i] == nums[i-1] ){
             continue;
         }
